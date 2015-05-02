740ea913e1e9bae87f4776fe23983984349e2f33
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-05-02 18:51:03 +0100

    Initial file setup

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt){
+
+  grunt.initConfig({
+    jshint: {
+      all: ['Gruntfile.js', 'app/**/*.js', 'spec/**/*.js', 'public/**/*.js']
+    },
+    watch: {
+      files: ['app/**/*.js', 'public/**/*.js', 'spec/**/*.js'],
+      tasks: ['jshint']
+    },
+    karma: {
+      unit: {
+        configFile: 'test/karma.conf.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.registerTask('jshint', ['jshint']);
+  grunt.registerTask('test', ['jshint', 'karma']);
+  grunt.registerTask('default');
+
+};
